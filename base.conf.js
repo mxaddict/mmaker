3638f9e536ecad944a85fea635c0555356cbaa28
Barry Deeney <mxaddict@codedmaster.com>
2017-12-21 12:59:13 +0800

    Updated the config file

---
+++ base.conf.js
@@ -0,0 +1,12 @@
+// What is our config?
+let conf = {
+  API: {
+    exchange: 'binance',
+    creds: {
+      key: '',    // You know what this is?
+      secret: ''  // If you don't please leave!
+    }
+  }
+}
+
+module.export = conf
