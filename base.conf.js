551f54766faf9877dbfca0f64d6ffb2220940f97
Barry Deeney <mxaddict@codedmaster.com>
2017-12-24 07:13:11 +0000

    Update base conf

--- base.conf.js
+++ base.conf.js
@@ -1,11 +1,19 @@
 // What is our config?
 let conf = {
-  exchange: 'binance',
-  market: 'XMR/BTC',
-  binance: {
+  exchange: 'bitfinex',
+  market: 'BCH/BTC',
+  minWidthPercent: 0.09,
+  minWidthPercentIncrement: 0.1,
+  maxBullets: 5,
+  pollInfo: 5000,
+  pollOrders: 5000,
+  pollReport: 5000,
+  positionPlTarget: 5,
+  bitfinex: {
     apiKey: '', // You know what this is?
     secret: ''  // If you don't please leave!
-  }
+  },
 }
 
 module.exports = conf
+
