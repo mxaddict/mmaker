68b025d41bb976fb590c7c3a9dc17b7c86ab37a1
Barry Deeney <mxaddict@codedmaster.com>
2017-12-24 16:14:45 +0000

    Added adjust spread

--- base.conf.js
+++ base.conf.js
@@ -2,13 +2,19 @@
 let conf = {
   exchange: 'bitfinex',
   market: 'BCH/BTC',
-  minWidthPercent: 0.15,
-  minWidthPercentIncrement: 0.1,
-  maxBullets: 5,
+
+  // Get the poll settings
   pollInfo: 5000,
   pollOrders: 5000,
   pollReport: 5000,
-  positionPlTarget: 5,
+
+  // Get order settings
+  adjustSpread: false,
+  maxBullets: 5,
+  minWidthPercent: 0.1,
+  minWidthPercentIncrement: 0.1,
+  positionPlTarget: 2,
+
   bitfinex: {
     apiKey: '', // You know what this is?
     secret: ''  // If you don't please leave!
