b7961762d887e906347eb35089097ceaef2d023e
Barry Deeney <mxaddict@codedmaster.com>
2017-12-25 06:10:45 +0000

    Larger default spread

--- config/sample.js
+++ config/sample.js
@@ -11,7 +11,7 @@ let conf = {
   // Get order settings
   adjustSpread: false,
   maxBullets: 5,
-  minWidthPercent: 0.1,
+  minWidthPercent: 0.15,
   minWidthPercentIncrement: 0.1,
   orderSize: 0, // ZERO = AUTO
   positionPlTarget: 5,
