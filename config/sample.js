92b07a92cda0b9b32a415a097ca148b34b89d408
Barry Deeney <mxaddict@codedmaster.com>
2017-12-29 07:44:27 +0000

    Fixed the position target issue

--- config/sample.js
+++ config/sample.js
@@ -11,14 +11,14 @@ let conf = {
   // Get order settings
   adjustSpread: true,
   minWidthPercent: 0.3,
-  minWidthPercentIncrement: 0.1,
+  minWidthPercentIncrement: 0.15,
   orderCountBuy: 5,
   orderCountBuyMin: 5,
   orderCountSell: 5,
   orderCountSellMin: 5,
   orderSize: 0, // ZERO = AUTO
   orderSizeMultiplier: 0.1,
-  positionTargets: [ 2 ],
+  positionTarget: 1,
   saveReport: true,
 
   bitfinex: {
