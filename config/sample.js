4ac9dfc0b88c7ee7f12a081d18650d48037bb3c8
Barry Deeney <mxaddict@codedmaster.com>
2018-04-25 16:54:02 +0800

    Updates for config

--- config/sample.js
+++ config/sample.js
@@ -4,20 +4,20 @@ let conf = {
   market: 'NAV/BTC',
 
   // Get the poll settings
-  pollInfo: 8000,
-  pollOrders: 8000,
+  pollInfo: 5000,
+  pollOrders: 5000,
   pollReport: 5000,
 
   // Get order settings
   adjustSpread: true,
-  minWidthPercent: 0.5,
-  minWidthPercentIncrement: 0.3,
-  orderCountBuy: 5,
-  orderCountBuyMin: 3,
-  orderCountSell: 5,
-  orderCountSellMin: 3,
+  minWidthPercent: 0.6,
+  minWidthPercentIncrement: 0.4,
+  orderCountBuy: 3,
+  orderCountBuyMin: 1,
+  orderCountSell: 3,
+  orderCountSellMin: 1,
   orderSize: 0, // ZERO = AUTO USES orderSizeMultiplier * accountBalance
-  orderSizeMultiplier: 5, // 0 - 100 % possible values
+  orderSizeMultiplier: 2.5, // 0 - 100 % possible values
   saveReport: true, // Saves a report file in ./public/report.json
 
   binance: {
